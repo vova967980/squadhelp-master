93510d909dc2878b1b4c88a8b319817167754ae7
Vasy Terkin <vasy.terkin12@gmail.com>
2020-03-30 16:26:24 +0300

    Fix his.props on this.props

--- client/src/components/ContestsContainer/ContestsContainer.js
+++ client/src/components/ContestsContainer/ContestsContainer.js
@@ -24,7 +24,7 @@ class ContestsContainer extends React.Component {
     };
 
     render() {
-        const {isFetching} = his.props;
+        const {isFetching} = this.props;
         if (!isFetching && this.props.children.length === 0) {
             return <div className={styles.notFound}>Nothing not found</div>;
         } else return (
