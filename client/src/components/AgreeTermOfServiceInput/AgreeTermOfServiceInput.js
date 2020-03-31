1bd722924d4bb3b4363376a67ca630f7fb56930c
VladimirS <vova967980@gmail.com>
2020-04-01 00:57:44 +0300

    Fix Term Services link. Open in new tab required

--- client/src/components/AgreeTermOfServiceInput/AgreeTermOfServiceInput.js
+++ client/src/components/AgreeTermOfServiceInput/AgreeTermOfServiceInput.js
@@ -7,7 +7,7 @@ const AgreeTermOfServiceInput = ({label, id,input, type, classes, meta: {touched
             <div className={classes.container}>
                 <input {...input} placeholder={label} id={id} type={type}/>
                 <label htmlFor={id}>By clicking this checkbox, you agree to our <a
-                    href="https://www.google.com">Terms of Service.</a></label>
+                    href="https://www.google.com" target="_blank">Terms of Service.</a></label>
             </div>
             {touched && (error && <span className={classes.warning}>{error}</span>)}
         </div>
